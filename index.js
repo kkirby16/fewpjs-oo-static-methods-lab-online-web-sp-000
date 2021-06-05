bc8ad54f2901b8fd6038c59479bcc8b0f1ce0399
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-05 02:45:38 -0700

    commit tree 4a777003ecc266268d003dd21cb357faa10cb29b

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^-' A-Za-z0-9]+/g, "");
+  }
+
+  static titleize(string) {
+    let arr = [];
+    let excludeWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    arr = string.split(" ");
+    return arr
+      .map((word, i) => {
+        console.log(i);
+        return excludeWords.includes(word) && i > 0 //checking if i > 0
+          ? [word]
+          : word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
 }
